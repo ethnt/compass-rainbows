d2660d1a09608178f3c4822bffab7e96c59f78e0
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-10-02 17:24:45 -0400

    fix tests.

--- test/config.rb
+++ test/config.rb
@@ -1,10 +1,10 @@
-require '../lib/rainbows.rb'
+require '../lib/compass-rainbows.rb'
 
 project_type = :stand_alone
 http_path = '/'
 sass_dir = 'sass'
 css_dir = 'css'
 line_comments = false
-preferred_syntax = :scss
+preferred_syntax = :sass
 output_style = :expanded
 relative_assets = true
