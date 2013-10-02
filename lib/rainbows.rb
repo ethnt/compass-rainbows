0c77f8c4c91235c3a009294b382b559a0e97a85a
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-10-02 16:18:32 -0400

    fixing some stuff.

--- lib/rainbows.rb
+++ lib/rainbows.rb
@@ -1,3 +1,5 @@
 require 'compass'
 
-Compass::Frameworks.register('rainbows', path: File.expand_path(File.join(File.dirname(__FILE__), '..'')))
+path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+Compass::Frameworks.register('my_extension', :path => path)
